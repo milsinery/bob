92e5e50693b1f0273f1ab19ebfee7c511f1ce050
Vladimir Bykov <milsinery@icloud.com>
2019-06-12 13:29:58 +0300

    add card component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Card extends React.Component {
+
+  render() {
+    return(
+      <div className="Card">
+        <h2 className="Tilte">{this.props.title}</h2>
+        <p className="Description">{this.props.description}</p>
+      </div> 
+    );
+  }
 }
 
-export default App;
+Card.defaultProps = {title: "Title", description: "Hello, React!"};
+
+export default Card;
