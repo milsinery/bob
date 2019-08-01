e87b62d8f135220fdc363f6f16123b665f1b81d8
Vladimir Bykov <milsinery@ip-143.office>
2019-08-01 10:38:09 +0300

    move create card code to function CreateCard

--- src/Cards.js
+++ src/Cards.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import Card from './Card';
 import CardsData from "./CardsData";
-
 import './Cards.css';
 
 class Cards extends React.Component {
 
     render(){
 
+        function createCard(data) {
+            return data.map(content => 
+                <Card 
+                    id={content.id} 
+                    title={content.title} 
+                    description={content.description}
+                    links={content.link.map(links => <a className="Nav__Item" href={links}>{links}</a>)}
+                />
+            )
+        }
+
         return(
             <div className="Cards">
-                {CardsData.map(content => 
-                    <Card 
-                        id={content.id} 
-                        title={content.title} 
-                        description={content.description}
-                        links={content.link.map(links => <a className="Nav__Item" href={links}>{links}</a>)}
-                    />
-                )}
+                {createCard(CardsData)}
             </div>
         );
     }
